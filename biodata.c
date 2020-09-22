d1bf484d41e71f42433bc5b08a82c69ad5320662
Rachmadany Iman <dekdhanymanofficial@gmail.com>
2020-09-22 21:09:53 +0700

    first commit

---
+++ biodata.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	printf("******* BIODATA *******\n\n");
+	printf("Nama	: Rachmadhany Iman\n");
+	printf("TTL	: Surabaya,01 Februari 2000\n");
+	printf("NPM	: 19081010142\n");
+	printf("Alamat	: Kalilom Lor indah nanas/14 \n");
+	
+	return 0;
+}
